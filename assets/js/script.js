971cec13b516983f3556ef7ecde21cb90dc31a8d
Nick Walton <nickwalton1989@outlook.com>
2023-03-21 11:36:10 +0000

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,7 +122,7 @@ function displayAdditionQuestion(operand1, operand2) {
 function displaySubtractQuestion(operand1, operand2) {
 
     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 
 }
@@ -136,6 +136,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
 
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
